bee9cad5e4e317c7a170dd3dbdaeb7ffe5125905
Christopher Mühl <christopher@dieschittigs.de>
2018-03-27 18:29:00 +0100

    Style administration page

--- src/components/administration/Administration.vue
+++ src/components/administration/Administration.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="component">
     <nav class="navigation">
       <router-link
         class="link"
         :to="{ name: 'admin' }"
         tag="div"
       >
-        <font-awesome-icon icon="home" size="2x" />
+        <font-awesome-icon icon="home" class="icon" /> Dashboard
       </router-link>
 
       <router-link
@@ -14,7 +14,7 @@
         :to="{ name: 'admin.devices' }"
         tag="div"
       >
-        <font-awesome-icon icon="microchip" size="2x" />
+        <font-awesome-icon icon="microchip" class="icon" /> Devices
       </router-link>
 
       <router-link
@@ -22,7 +22,7 @@
         :to="{ name: 'admin.users' }"
         tag="div"
       >
-        <font-awesome-icon icon="users" size="2x" />
+        <font-awesome-icon icon="users" class="icon" /> Users
       </router-link>
 
       <router-link
@@ -30,7 +30,7 @@
         :to="{ name: 'admin.extensions' }"
         tag="div"
       >
-        <font-awesome-icon icon="plug" size="2x" />
+        <font-awesome-icon icon="plug" class="icon" /> Extensions
       </router-link>
 
       <router-link
@@ -38,11 +38,11 @@
         :to="{ name: 'admin.settings' }"
         tag="div"
       >
-        <font-awesome-icon icon="cogs" size="2x" />
+        <font-awesome-icon icon="cogs" class="icon" /> Settings
       </router-link>
     </nav>
 
-    <router-view />
+    <router-view class="content" />
   </div>
 </template>
 
@@ -61,21 +61,48 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.component {
+  height: 100%;
+}
+
+.content {
+  height: 100%;
+  padding: 20px 40px;
+  background: #f6f8fa;
+  text-align: left;
+}
+
 .navigation {
   display: flex;
   flex-direction: row;
   justify-content: center;
+  border-bottom: 1px solid #E1E4F0;
 
   .link {
-    width: 80px;
-    height: 60px;
+    height: 50px;
+    margin-right: 8px;
+    padding: 0 8px;
+
     display: flex;
     justify-content: center;
     align-items: center;
+
+    transition: all 0.1s;
     cursor: pointer;
+
+    .icon {
+      margin-right: 5px;
+    }
 
     &:hover {
       color: #3D546B;
+      border-bottom: 1px solid lighten(#3D546B, 30%);
+    }
+
+    &:last-of-type { margin-right: 0; }
+
+    &.router-link-exact-active {
+      border-bottom: 1px solid orange;
     }
   }
 }
