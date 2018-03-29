7fb09923d33c5717c3583167b96a9c615072f747
Christopher Mühl <christopher@dieschittigs.de>
2018-03-29 16:25:42 +0200

    Don't force to login for now

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   if (to.name !== 'login' && !store.state.authentication.jwtToken) {
-    next({ name: 'login' })
+  //  next({ name: 'login' })
   }
 
   next()
